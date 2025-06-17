867f89f2041b4c52bd2f1478e4fcafe1806c258d
sachin shivade <sachinrs2747@gmail.com>
2025-06-17 22:49:09 +0530

    first commit

---
+++ src/contents/projects.ts
@@ -0,0 +1,61 @@
+import { Project } from "@/types";
+
+
+export const projects: Project[] = [
+    {
+        title: 'Portfolio Website',
+        description: 'My personal portfolio website showcasing my projects and skills.',
+        technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
+        // githubLink: 'https://github.com',
+        demoLink: 'https://www.demo.com/',
+        image: '/projects/Portfolio.png',
+      },
+      {
+        title: 'Personal Portfolio Website for Upamanyu Jondhale',
+        description: 'Developed a responsive personal portfolio website for a software developer to showcase projects, skills, and contact details.',
+        technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
+        // githubLink: 'https://github.com',
+        demoLink: 'https://www.demo.com/',
+        image: '/projects/Upamanyu-portfolio.png',
+      },
+      {
+        title: 'MechManager',
+        description: 'Allows users to register, update, and track vehicles in the system, including details like make, model, year, VIN, and service history. Enables mechanics and service center managers to create and assign repair tasks, schedule maintenance, and track the progress of each task. Stores and manages customer information, including contact details, service history, and billing information, with secure access controls.',
+        technologies: ['Python','Flask', 'Vue.js', 'JavaScript', 'Tailwind CSS'],
+        // githubLink: 'https://github.com',
+        demoLink: 'https://www.mechmanager.com/',
+        image: '/projects/MechManager.png',
+      },
+      {
+        title: 'AssetTracker and Tracking Device',
+        description: 'Staff can accept, attend, or reject calls, and submit notes after attending. On login, they select a zone and role (staff or patient alerts), with all actions and alerts confirmed and logged for tracking.',
+        technologies: ['Python', 'Flask', 'WebSocket.io', 'Vue.js', 'JavaScript', 'Tailwind CSS'],
+        // githubLink: 'https://github.com',
+        demoLink: 'https://tdfront.thermalcode.com/login',
+        image: '/projects/SmartSense.jpg',
+      },
+      {
+        title: 'All Pencils Down',
+        description: 'Combines Laravel for backend functionality with Vue.js for frontend interactivity. An online platform for purchasing courses, providing a secure login and easy browsing, purchase, and management of courses built with Laravel and Vue.js.',
+        technologies: ['Laravel', 'Vue.js', 'JavaScript', 'Tailwind CSS'],
+        // githubLink: 'https://github.com',
+        demoLink: 'https://www.allpencilsdown.com/',
+        image: '/projects/allpencilsdown.png',
+      },
+      {
+        title: 'Real-Estate-Aqar',
+        description: 'A project developed with PHP Laravel and Vue.js typically combines the backend functionalities handled by Laravel with the frontend interactivity and user interface enhancements provided by Vue.js. This project is used for online Sale and Buy Properties and interaction between the Agents and members who post there properties on the application. Objective of this application is to make it interactive and its ease of use.',
+        technologies: ['Laravel', 'Vue.js', 'JavaScript', 'Tailwind CSS'],
+        // githubLink: 'https://github.com',
+        demoLink: 'https://beta.uae.estate/',
+        image: '/projects/Real-Estate-Aqar.png',
+      },
+      {
+        title: 'Resolved CRM',
+        description: 'It is a dynamic web application developed using PHP Laravel on the backend and Vue.js on the frontend. As a backend developer working with Laravel, my role involves designing and implementing robust server side applications and APIs. I am responsible for creating and maintaining database schemas, ensuring data integrity, and optimizing performance.',
+        technologies: ['Laravel', 'Vue.js', 'JavaScript', 'Tailwind CSS'],
+        // githubLink: 'https://github.com',
+        demoLink: 'https://new-diamondrx.resolvecrm.com/',
+        image: '/projects/ResolveCRM.png',
+      },
+  ];
